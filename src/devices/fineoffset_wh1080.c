da75087856560ca81b1b006728ecb35dd6003740
jfarcher <jf_archer@yahoo.com>
2018-02-27 15:16:00 +0000

    Removed elements to compile without BMP... unclean as yet but will work on it.

--- src/devices/fineoffset_wh1080.c
+++ src/devices/fineoffset_wh1080.c
@@ -98,7 +98,7 @@
 #include <stdlib.h>
 
 #include <unistd.h>
-#include <linux/i2c-dev.h>
+//#include <linux/i2c-dev.h>
 //#include <linux/i2c.h>
 #include <sys/ioctl.h>
 
@@ -106,10 +106,10 @@
 #define CRC_POLY 0x31
 #define CRC_INIT 0xff
 
-#define BMP085_I2C_ADDRESS 0x77
+//#define BMP085_I2C_ADDRESS 0x77
 
 
-const double station_altitude = 10;  // <----- Edit this value entering YOUR station altitude!
+const double station_altitude = 300;  // <----- Edit this value entering YOUR station altitude!
 
 const unsigned char BMP085_OVERSAMPLING_SETTING = 3;
 
@@ -131,206 +131,6 @@ int b5;
 
 unsigned int temperature, pressure;
 
-
-// Open a connection to the bmp085
-// Returns a file id
-int bmp085_i2c_Begin()
-{
-	int fd;
-	char *fileName = "/dev/i2c-1"; //<------- If your Raspberry is an older model and pressure doesn't work, 
-	// try changing '1' to '0'. Also change it to '2' if you are using a BananaPi! ("/dev/i2c-2";)
-	
-	// Open port for reading and writing
-	if ((fd = open(fileName, O_RDWR)) < 0)
-		exit(1);
-	
-	// Set the port options and set the address of the device
-	if (ioctl(fd, I2C_SLAVE, BMP085_I2C_ADDRESS) < 0) {					
-		close(fd);
-		exit(1);
-	}
-
-	return fd;
-}
-
-// Read two words from the BMP085 and supply it as a 16 bit integer
-__s32 bmp085_i2c_Read_Int(int fd, __u8 address)
-{
-	__s32 res = i2c_smbus_read_word_data(fd, address);
-	if (res < 0) {
-		close(fd);
-		exit(1);
-	}
-
-	// Convert result to 16 bits and swap bytes
-	res = ((res<<8) & 0xFF00) | ((res>>8) & 0xFF);
-
-	return res;
-}
-
-//Write a byte to the BMP085
-void bmp085_i2c_Write_Byte(int fd, __u8 address, __u8 value)
-{
-	if (i2c_smbus_write_byte_data(fd, address, value) < 0) {
-		close(fd);
-		exit(1);
-	}
-}
-
-// Read a block of data BMP085
-void bmp085_i2c_Read_Block(int fd, __u8 address, __u8 length, __u8 *values)
-{
-	if(i2c_smbus_read_i2c_block_data(fd, address,length,values)<0) {
-		close(fd);
-		exit(1);
-	}
-}
-
-
-void bmp085_Calibration()
-{
-	int fd = bmp085_i2c_Begin();
-	ac1 = bmp085_i2c_Read_Int(fd,0xAA);
-	ac2 = bmp085_i2c_Read_Int(fd,0xAC);
-	ac3 = bmp085_i2c_Read_Int(fd,0xAE);
-	ac4 = bmp085_i2c_Read_Int(fd,0xB0);
-	ac5 = bmp085_i2c_Read_Int(fd,0xB2);
-	ac6 = bmp085_i2c_Read_Int(fd,0xB4);
-	b1 = bmp085_i2c_Read_Int(fd,0xB6);
-	b2 = bmp085_i2c_Read_Int(fd,0xB8);
-	mb = bmp085_i2c_Read_Int(fd,0xBA);
-	mc = bmp085_i2c_Read_Int(fd,0xBC);
-	md = bmp085_i2c_Read_Int(fd,0xBE);
-	close(fd);
-}
-
-// Read the uncompensated temperature value
-unsigned int bmp085_ReadUT()
-{
-	unsigned int ut = 0;
-	int fd = bmp085_i2c_Begin();
-
-	// Write 0x2E into Register 0xF4
-	// This requests a temperature reading
-	bmp085_i2c_Write_Byte(fd,0xF4,0x2E);
-	
-	// Wait at least 4.5ms
-	usleep(5000);
-
-	// Read the two byte result from address 0xF6
-	ut = bmp085_i2c_Read_Int(fd,0xF6);
-
-	// Close the i2c file
-	close (fd);
-	
-	return ut;
-}
-
-// Read the uncompensated pressure value
-unsigned int bmp085_ReadUP()
-{
-	unsigned int up = 0;
-	int fd = bmp085_i2c_Begin();
-
-	// Write 0x34+(BMP085_OVERSAMPLING_SETTING<<6) into register 0xF4
-	// Request a pressure reading w/ oversampling setting
-	bmp085_i2c_Write_Byte(fd,0xF4,0x34 + (BMP085_OVERSAMPLING_SETTING<<6));
-
-	// Wait for conversion, delay time dependent on oversampling setting
-	usleep((2 + (3<<BMP085_OVERSAMPLING_SETTING)) * 1000);
-
-	// Read the three byte result from 0xF6
-	// 0xF6 = MSB, 0xF7 = LSB and 0xF8 = XLSB
-	__u8 values[3];
-	bmp085_i2c_Read_Block(fd, 0xF6, 3, values);
-
-	up = (((unsigned int) values[0] << 16) | ((unsigned int) values[1] << 8) | (unsigned int) values[2]) >> (8-BMP085_OVERSAMPLING_SETTING);
-
-	// Close the i2c file
-	close (fd);
-	
-	return up;
-}
-
-// Calculate pressure given uncalibrated pressure
-// Value returned will be in units of Pa
-unsigned int bmp085_GetPressure(unsigned int up)
-{
-	int x1, x2, x3, b3, b6, p;
-	unsigned int b4, b7;
-  
-	b6 = b5 - 4000;
-	// Calculate B3
-	x1 = (b2 * (b6 * b6)>>12)>>11;
-	x2 = (ac2 * b6)>>11;
-	x3 = x1 + x2;
-	b3 = (((((int)ac1)*4 + x3)<<BMP085_OVERSAMPLING_SETTING) + 2)>>2;
-  
-	// Calculate B4
-	x1 = (ac3 * b6)>>13;
-	x2 = (b1 * ((b6 * b6)>>12))>>16;
-	x3 = ((x1 + x2) + 2)>>2;
-	b4 = (ac4 * (unsigned int)(x3 + 32768))>>15;
-  
-	b7 = ((unsigned int)(up - b3) * (50000>>BMP085_OVERSAMPLING_SETTING));
-	if (b7 < 0x80000000)
-		p = (b7<<1)/b4;
-	else
-		p = (b7/b4)<<1;
-	
-	x1 = (p>>8) * (p>>8);
-	x1 = (x1 * 3038)>>16;
-	x2 = (-7357 * p)>>16;
-	p += (x1 + x2 + 3791)>>4;
-  
-	return p;
-}
-
-// Calculate temperature given uncalibrated temperature
-// Value returned will be in units of 0.1 deg C
-unsigned int bmp085_GetTemperature(unsigned int ut)
-{
-	int x1, x2;
-  
-	x1 = (((int)ut - (int)ac6)*(int)ac5) >> 15;
-	x2 = ((int)mc << 11)/(x1 + md);
-	b5 = x1 + x2;
-
-	unsigned int result = ((b5 + 8)>>4);  
-
-	return result;
-}
-
-
-// --------- Get PRESSURE -------------------------------------------------------------------
-
-
-double read_int_temp()
-{
-	bmp085_Calibration();
-	temperature = bmp085_GetTemperature(bmp085_ReadUT());
-	pressure = bmp085_GetPressure(bmp085_ReadUP());
-	
-	return ((double)temperature)/10;	
-}
-
-
-double read_press()
-{
-	bmp085_Calibration();
-	temperature = bmp085_GetTemperature(bmp085_ReadUT());
-	pressure = bmp085_GetPressure(bmp085_ReadUP());
-	return ((((double)pressure)/100) / pow((1.0 - (station_altitude/100)/44330.0), 5.255));
-	//return ((((double)pressure)/100)/ pow(1.0 - station_altitude/44330.0, 5.255));
-	
-	//Relative pressure calculated from 'station_altitude' value. See https://en.wikipedia.org/wiki/Barometric_formula 
-	//See also: https://www.mkompf.com/weather/pibaro.html
-	//
-	//Remember to change 'station_altitude' value at the top of this file to reflect YOUR station altitude!
-}
-
-
-//-------------------------------------------------------------------------------------------
 
 
 unsigned short msg_type = 0; // 0=Weather   1=Time
@@ -486,8 +286,6 @@ static int fineoffset_wh1080_callback(bitbuffer_t *bitbuffer) {
     	const int humidity = get_humidity(br);
     	const char* direction_str = get_wind_direction_str(br);
     	const char* direction_deg = get_wind_direction_deg(br);	
-    	const float pressure = read_press();
-    	const float int_temp = read_int_temp();
 	
 	// Select which metric system for *wind avg speed* and *wind gust* :
 	
@@ -531,13 +329,11 @@ if (msg_type == 0) {
                      "id",            		"StationID",		DATA_FORMAT, "%04X",	DATA_INT,    device_id,
                      "temperature_C", 		"Temperature",		DATA_FORMAT, "%.01f C",	DATA_DOUBLE, temperature,
                      "humidity",      		"Humidity",		DATA_FORMAT, "%u %%",	DATA_INT,    humidity,
-		     "pressure", 		"Pressure",		DATA_FORMAT, "%.02f hPa",	DATA_DOUBLE, pressure,
                      "direction_str", 		"Wind string",		DATA_STRING, direction_str,
 		     "direction_deg", 		"Wind degrees",		DATA_STRING, direction_deg,
                      "speed",         		"Wind avg speed",	DATA_FORMAT, "%.02f",	DATA_DOUBLE, speed,
                      "gust",          		"Wind gust",		DATA_FORMAT, "%.02f",	DATA_DOUBLE, gust,
                      "rain",          		"Total rainfall",	DATA_FORMAT, "%.01f",	DATA_DOUBLE, rain,
-		     "int_temp",	  	"Internal temp.",	DATA_FORMAT, "%.01f C",	DATA_DOUBLE, int_temp,
 		     //"battery",	  	"Battery",		DATA_STRING, battery, // Unsure about Battery byte...
                      NULL);
     data_acquired_handler(data);
